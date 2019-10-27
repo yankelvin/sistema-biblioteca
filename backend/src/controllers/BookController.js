d3e5f60d9440d48f6bc2996f87abdda3f599c286
rendmp <pedro.barbosa.dev@gmail.com>
2019-10-27 13:41:46 -0300

    Adicionado validação  para a criação de livro na função store da Api utilizando a biblioteca Ajv.

--- backend/src/controllers/BookController.js
+++ backend/src/controllers/BookController.js
@@ -1,25 +1,40 @@
 const Book = require("../models/Book");
+const Ajv = require("ajv");
 
 module.exports = {
   async store(req, res) {
-    const {
-      name,
-      author,
-      publisher,
-      yearPublication,
-      area,
-      purchaseDate,
-      pricePaid
-    } = req.body;
-    const book = await Book.create({
-      name,
-      author,
-      publisher,
-      yearPublication,
-      area,
-      purchaseDate,
-      pricePaid
-    });
+    const ajv = new Ajv({ allErrors: true });
+
+    var schema = {
+      properties: {
+        name: { type: "string", maxLength: 50, minLength: 5 },
+        author: { type: "string", maxLength: 50, minLength: 5 },
+        publisher: { type: "string", maxLength: 50, minLength: 5 },
+        yearPublication: { type: "number" },
+        area: { type: "string", maxLength: 20, minLength: 5 },
+        purchaseDate: { type: "number" },
+        pricePaid: { type: "number" }
+      }
+    };
+
+    var validate = ajv.compile(schema);
+
+    if (!validate(req.body)) {
+      return res.json({
+        400: `O pedido não pôde ser entregue devido à sintaxe incorreta. ${ajv.errorsText(
+          validate.errors
+        )}`
+      });
+    }
+
+    const bookExists = await Book.findOne({ where: { name: req.body.name } });
+
+    if (bookExists) {
+      return res.status(400).json({ 400: `${req.body.name} already exists` });
+    }
+
+    const book = await Book.create(req.body);
+
     return res.json({ book });
   },
   async index(req, res) {
@@ -28,15 +43,7 @@ module.exports = {
   },
   async update(req, res) {
     const query = { _id: req.body._id };
-    const {
-      name,
-      author,
-      publisher,
-      yearPublication,
-      area,
-      purchaseDate,
-      pricePaid
-    } = req.body;
+    const { name, author, publisher, yearPublication, area, purchaseDate, pricePaid } = req.body;
     const book = await Book.updateOne(
       query,
       {
