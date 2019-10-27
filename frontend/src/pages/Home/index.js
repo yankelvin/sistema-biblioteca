bd5616581ea963f72824dd10967207cc4b0d8e83
rendmp <pedro.barbosa.dev@gmail.com>
2019-10-26 22:38:50 -0300

    Adicionada função para criação dos livros por  formulario. Modificações na estrutura de pasta  do frontend

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,110 @@
+// React
+import React, { Component } from "react";
+
+// Material Ui
+import Button from "@material-ui/core/Button";
+
+// Router
+import { Link } from "react-router-dom";
+
+//Utils
+import api from "../../services/api";
+
+export default class Home extends Component {
+  state = {
+    feed: []
+  };
+
+  async componentDidMount() {
+    const response = await api.get("books");
+    this.setState({ feed: response.data });
+  }
+
+  render() {
+    return (
+      <>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
+        ></link>
+        <script
+          src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+          integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+          integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+          crossorigin="anonymous"
+        ></script>
+        <script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+          integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+          crossorigin="anonymous"
+        ></script>
+        <br />
+        <br />
+        <div align="center">
+          <h3>Lista de Livros Disponíveis</h3>
+
+          <Link to={"/new/employee"}>
+            <Button href="/new/employee" variant="contained" color="primary" size="small">
+              Cadastro de Funcionários
+            </Button>
+          </Link>
+
+          <Link to={"/new/book"}>
+            <Button variant="contained" color="primary" size="small">
+              {" "}
+              Cadastro de Livros
+            </Button>
+          </Link>
+        </div>
+
+        <main role="main">
+          <div class="album py-5 bg-light">
+            <div class="container">
+              <div class="row">
+                {this.state.feed.map(book => (
+                  <div class="col-md-4">
+                    <div class="card mb-4 shadow-sm">
+                      <img class="card-img-top" alt="Card image cap" />
+                      <div class="card-body">
+                        <strong class="d-inline-block mb-2 text-success">{this.props.area}</strong>
+
+                        <p class="card-text">
+                          <a class="text-dark" href="#">
+                            {" "}
+                            <b> {book.name}</b>
+                          </a>
+                          <br />
+                          <div class="mb-1 text-muted">
+                            por {book.author} | {book.purchaseDate}
+                          </div>
+                        </p>
+
+                        <p>R$ {this.props.pricePaid}</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                          <div class="btn-group">
+                            <Link to={""}>
+                              <button type="button" class="btn btn-sm btn-outline-secondary">
+                                Ver
+                              </button>
+                            </Link>
+                          </div>
+                          <small class="text-muted">9 mins</small>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </main>
+      </>
+    );
+  }
+}
---
+++ frontend/src/pages/Livros/CadastroLivros.js
@@ -0,0 +1,164 @@
+import React, { Fragment } from "react";
+
+//Material-ui
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+// Utils
+import api from "../../services/api";
+
+//
+import BotaoSucesso from "../../components/BotaoSucesso";
+
+export default class CreateBook extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      image: null,
+      name: "",
+      author: "",
+      publisher: "",
+      yearPublication: "",
+      area: "",
+      purchaseDate: "",
+      pricePaid: ""
+    };
+  }
+
+  handleImage = event => {
+    this.setState({ image: event.target.files[0] });
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+    console.log(event.target.name, event.target.value);
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    const data = new FormData();
+
+    data.append("image", this.state.image);
+    data.append("name", this.state.name);
+    data.append("author", this.state.author);
+    data.append("publisher", this.state.publisher);
+    data.append("yearPublication", this.state.yearPublication);
+    data.append("area", this.state.area);
+    data.append("purchaseDate", this.state.purchaseDate);
+    data.append("pricePaid", this.state.pricePaid);
+
+    const response = await api.post("new/book", data);
+
+    this.props.history.push("/");
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <div align="center">
+          <br />
+          <br />
+          <code>
+            <h2>Cadastre um Livro</h2>
+          </code>
+          <form onSubmit={this.handleSubmit}>
+            <input type="file" onChange={this.handleImage} />
+            <br />
+
+            <TextField
+              name="name"
+              id="name"
+              label="Nome do Livro"
+              onChange={this.handleChange}
+              margin="normal"
+              variant="standard"
+              style={{ width: 550 }}
+            />
+            <br />
+            <TextField
+              name="author"
+              id="author"
+              label="Nome do Autor"
+              onChange={this.handleChange}
+              margin="normal"
+              variant="standard"
+              style={{ width: 550 }}
+            />
+            <br />
+            <TextField
+              name="publisher"
+              id="publisher"
+              label="Nome da Editora"
+              onChange={this.handleChange}
+              margin="normal"
+              variant="standard"
+              style={{ width: 550 }}
+            />
+
+            <br />
+            <TextField
+              name="area"
+              id="area"
+              label="Área de conhecimento"
+              onChange={this.handleChange}
+              margin="normal"
+              variant="standard"
+              style={{ width: 550 }}
+            />
+
+            <br />
+
+            <br />
+            <TextField
+              name="yearPublication"
+              id="yearPublication"
+              label="Ano de Publicação"
+              onChange={this.handleChange}
+              margin="normal"
+              variant="standard"
+              style={{ width: 550 }}
+            />
+
+            <br />
+            <TextField
+              name="purchaseDate"
+              id="purchaseDate"
+              label="Data de Compra"
+              onChange={this.handleChange}
+              margin="normal"
+              variant="standard"
+              style={{ width: 550 }}
+            />
+
+            <br />
+
+            <InputLabel htmlFor="preco" style={{ marginRight: 450 }}>
+              Preço do Livro
+            </InputLabel>
+
+            <Input
+              name="pricePaid"
+              id="pricePaid"
+              type="number"
+              onChange={this.handleChange}
+              startAdornment={<InputAdornment position="start">$</InputAdornment>}
+              style={{ marginRight: 310, width: 240 }}
+            />
+
+            <BotaoSucesso />
+
+            <Button href="/" color="primary">
+              Cancelar
+            </Button>
+
+            <br />
+          </form>
+        </div>
+      </Fragment>
+    );
+  }
+}
