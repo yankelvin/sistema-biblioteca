6c0b7177ccbcf8fe1d090e02c134d75e9cb38c90
rendmp <pedro.barbosa.dev@gmail.com>
2019-10-28 12:38:55 -0300

    Adicionado recebimento de imagem via req.file  e manipualção de imagens com a biblioteca sharp.

--- backend/src/controllers/BookController.js
+++ backend/src/controllers/BookController.js
@@ -1,24 +1,30 @@
 const Book = require("../models/Book");
 const Ajv = require("ajv");
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
 
 module.exports = {
   async store(req, res) {
+    const { name, author, publisher, yearPublication, area, purchaseDate, pricePaid } = req.body;
     const ajv = new Ajv({ allErrors: true });
 
     var schema = {
       properties: {
         name: { type: "string", maxLength: 50, minLength: 5 },
         author: { type: "string", maxLength: 50, minLength: 5 },
+        image: { type: "string" },
         publisher: { type: "string", maxLength: 50, minLength: 5 },
-        yearPublication: { type: "number" },
+        yearPublication: { type: "string" },
         area: { type: "string", maxLength: 20, minLength: 5 },
-        purchaseDate: { type: "number", min: 0 },
-        pricePaid: { type: "number", min: 0 }
+        purchaseDate: { type: "string" },
+        pricePaid: { type: "string" }
       }
     };
 
     var validate = ajv.compile(schema);
 
+    // Validações
     if (!validate(req.body)) {
       return res.json({
         400: `O pedido não pôde ser entregue devido à sintaxe incorreta. ${ajv.errorsText(
@@ -27,12 +33,33 @@ module.exports = {
       });
     }
 
-    const bookExists = await Book.findOne({ name: req.body.name });
+    const bookExists = await Book.findOne({ name: name });
+
     if (bookExists) {
-      return res.status(400).json({ error: `${req.body.name} already exists` });
+      return res.status(400).json({ error: `${name} already exists` });
     }
 
-    const book = await Book.create(req.body);
+    const { filename: image } = req.file;
+    const [imageName] = image.split(".");
+    const fileName = `${imageName}.jpg`;
+
+    await sharp(req.file.path)
+      .resize(50, 50)
+      .png({ quality: 100 })
+      .toFile(path.resolve(req.file.destination, "resized", fileName));
+
+    fs.unlinkSync(req.file.path);
+
+    const book = await Book.create({
+      name,
+      author,
+      publisher,
+      yearPublication,
+      image: fileName,
+      area,
+      purchaseDate,
+      pricePaid
+    });
 
     return res.json({ book });
   },
