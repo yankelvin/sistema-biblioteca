63feabe9152d11df42587cc387e9c24036df8953
Adler Kesley <adler.kesley@grupoviadigital.com>
2019-11-13 17:15:50 -0300

    ajuste tela de login

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -49,63 +49,58 @@ export default class Login extends Component {
 
   render() {
     const containerStyle = {
-      margin: "-55px auto",
-      maxWidth: "350px",
-      height: "100%",
-      width: "100%",
-      display: "flex",
-      flexDirection: " column",
-      alignItems: "center",
-      justifyContent: "center"
+      margin: '30px auto 0',
+      maxWidth: '350px',
+      width: '100%',
+      display: 'flex',
+      flexDirection: ' column',
+      alignItems: 'center'
     };
-
     const contentStyle = {
-      width: "100%",
-      backgroundImage: "white",
-      marginTop: "20px",
-      borderRadius: "8px",
-      padding: "30px",
-      border: "1px solid gray"
-    };
+      width: '100%',
+      backgroundImage: 'linear-gradient(to right,  #2980b9, #2c3e50)',
+      marginTop: '20px',
+      borderRadius: '8px',
+      padding: '30px',
+      boxShadow: '0.6em 0.6em rgba(148, 169, 236, 0.37)'
 
+
+    };
     const contentLabelStyle = {
-      fontSize: "14px",
-      color: "#fff",
-      fontWeight: "normal",
-      marginBottom: "8px",
-      fontFamily: "Roboto, Arial, Helvetica, sans-serif"
+      fontSize: '14px',
+      color: '#fff',
+      fontWeight: 'normal',
+      marginBottom: '8px',
+      fontFamily: 'Roboto, Arial, Helvetica, sans-serif',
     };
-
     const formStyle = {
-      display: "flex",
-      flexDirection: "column"
+      display: 'flex',
+      flexDirection: 'column'
     };
 
     const inputStyle = {
-      marginBottom: "20px",
-      border: "1px solid gray",
-      borderRadius: " 6px",
-      height: "45px",
-      padding: "0px 15px",
-      fontSize: "16px"
+      marginBottom: '20px',
+      border: '1px solid rgb(52, 68, 163)',
+      borderRadius: ' 6px',
+      height: '45px',
+      padding: '0px 15px',
+      fontSize: '16px'
     };
-
     const buttonStyle = {
       border: 0,
-      borderRadius: "4px",
-      width: "100%",
-      height: "42px",
-      padding: " 0 20px",
-      fontSize: "16px",
-      fontWeight: " bold",
-      background: "#326da8",
-      color: "white",
-      cursor: "pointer"
+      borderRadius: '4px',
+      width: '100%',
+      height: '42px',
+      padding: ' 0 20px',
+      fontSize: '16px',
+      fontWeight: ' bold',
+      background: 'rgb(32, 39, 88)',
+      color: 'white',
+      cursor: 'pointer'
     };
-
     const img = {
-      maxWidth: "30%"
-    };
+      maxWidth: '30%'
+    }
 
     return (
       <div className="container" style={containerStyle} onMouseOver={this.onlyAdm}>
